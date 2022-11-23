307df59bc2cc52e2b1271bea9fbb5960e13dd52d
Doyun Lee <nunnunkoip@gmail.com>
2022-11-23 11:49:05 +0900

    221123 이도윤

--- Assets/01.Scripts/PlatformSpawner.cs
+++ Assets/01.Scripts/PlatformSpawner.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] platforms;
+
+    public int stagePlatformsUnitCount;
+
+    float width; // 플랫폼의 가로 길이
+
+    void Awake()
     {
+        //width = ;
+    }
+
+    void OnEnable()
+    {
+        SetVariables();
+        SetOrder();
+    }
+
+    void Update()
+    {
+        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을 때 위치를 재배치
+        if (this.transform.position.x <= -width)
+        {
+            Reposition();
+        }
+    }
+
+    void SetVariables()
+    {
+        stagePlatformsUnitCount = 2;
+    }
+
+    void SetOrder()
+    {
+        int totalPlatformCount = platforms.Length;
+        
+        for(int i = 0; i < stagePlatformsUnitCount*4; i++)
+        {
+            int randNumber = Random.Range(0, totalPlatformCount);
+            //Instantiate<>
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void SpawnPlatform()
     {
-        
+
+    }
+
+    // 위치를 재배치하는 메서드
+    void Reposition()
+    {
+        // 현재 위치에서 오른쪽으로 가로 길이 * 2만큼 이동
+        Vector2 offset = new Vector2(width * 2f, 0);
+        this.transform.position = (Vector2)this.transform.position + offset;
     }
 }
