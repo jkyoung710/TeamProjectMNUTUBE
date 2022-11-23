1881416105b39c4d264874a9a28122c10407e86d
Doyun Lee <nunnunkoip@gmail.com>
2022-11-23 10:21:30 +0900

    20221123 이도윤 스피드 관련 작성

---
+++ Assets/01.Scripts/DeadZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// DeadZone과 충돌하면 게임오버가 되는 스크립트
+public class DeadZone : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            MainGameManager.instance.BeGameOver();
+        }
+    }
+}
--- Assets/01.Scripts/ScrollingObject.cs
+++ Assets/01.Scripts/ScrollingObject.cs
@@ -6,16 +6,40 @@ using UnityEngine;
 public class ScrollingObject : MonoBehaviour
 {
     public float speed = 10f; // 이동 속도
+    float updateSpeed;
+
+    void OnEnable()
+    {
+        SetVariables(); // 변수 초기화
+
+        // 이벤트에 등록. PlayerDead 이벤트 발행 시 이동 속도 0으로
+        MainEventBus.Subscribe(MainEventType.PlayerDead, SetVariables);
+        // 이벤트에 등록. PlayerOnGround 이벤트 발행 시 이동 속도 설정 값으로
+        MainEventBus.Subscribe(MainEventType.PlayerOnGround, Move);
+    }
+
+    void OnDisable()
+    {
+        // 이벤트 해제
+        MainEventBus.Unsubscribe(MainEventType.PlayerDead, SetVariables);
+        MainEventBus.Unsubscribe(MainEventType.PlayerOnGround, Move);
+    }
 
     void Update()
     {
         // 게임 오브젝트를 일정 속도로 왼쪽으로 평행이동하는 처리
 
-        // 게임오버가 아니라면
-        if (!MainGameManager.instance.isGameover)
-        {
-            // 초당 speed의 속도로 왼쪽으로 평행이동
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
+        // 초당 speed의 속도로 왼쪽으로 평행이동
+        transform.Translate(Vector3.left * updateSpeed * Time.deltaTime);
+    }
+
+    void SetVariables()
+    {
+        updateSpeed = 0;
+    }
+
+    void Move()
+    {
+        updateSpeed = speed;
     }
 }
