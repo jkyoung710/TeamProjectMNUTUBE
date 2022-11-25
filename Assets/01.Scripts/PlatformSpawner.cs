955d9b178cfa6184b9f89befba4bddb49d7eb94f
Doyun Lee <nunnunkoip@gmail.com>
2022-11-25 10:43:40 +0900

    221125 이도윤 발판생성

--- Assets/01.Scripts/PlatformSpawner.cs
+++ Assets/01.Scripts/PlatformSpawner.cs
@@ -5,62 +5,92 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     public GameObject[] platforms;
+    int[] platformOrder;
 
     public int stagePlatformsUnitCount;
+    int stageLength;
 
-    float width; // 플랫폼의 가로 길이
+    public float width; // 플랫폼의 가로 길이
 
     // 초반에 생성한 발판을 화면 밖에 숨겨둘 위치
     Vector2 poolPosition = new Vector2(0, -25);
 
+    public GameObject[] currentPlatforms;
+    public int currentIndex;
+
+    public GameObject startPlatform;
+    float platformPositionY;
+
     void Awake()
     {
-        //width = ;
+        width = startPlatform.transform.localScale.x;
+        platformPositionY = startPlatform.transform.position.y;
     }
 
     void OnEnable()
     {
         SetVariables();
         SetOrder();
-    }
-
-    void Update()
-    {
-        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을 때 위치를 재배치
-        if (this.transform.position.x <= -width)
-        {
-            Reposition();
-        }
+        SpawnPlatforms();
+        RepositionPlatforms();
     }
 
     void SetVariables()
     {
         stagePlatformsUnitCount = 2;
+        stageLength = (stagePlatformsUnitCount * 4) + 1;
+        currentIndex = 0;        
     }
 
     void SetOrder()
     {
         int totalPlatformCount = platforms.Length;
-        
-        for(int i = 0; i < stagePlatformsUnitCount*4; i++)
+        platformOrder = new int[stageLength];
+
+        for (int i = 1; i < stageLength; i++)
         {
-            int randNumber = Random.Range(0, totalPlatformCount);
-            Instantiate(platforms[randNumber], poolPosition, this.transform.rotation, this.transform);
-        }
-        
+            int randNumber = Random.Range(0, totalPlatformCount);            
+            platformOrder[i] = randNumber;            
+        }        
     }
 
-
-    void SpawnPlatform()
+    void SpawnPlatforms()
     {
+        currentPlatforms = new GameObject[stageLength];
+        currentPlatforms[0] = startPlatform;
 
+        for (int i = 1; i < stageLength; i++)
+        {
+            currentPlatforms[i] = Instantiate(platforms[platformOrder[i]], poolPosition, this.transform.rotation, this.transform);
+        }        
     }
 
     // 위치를 재배치하는 메서드
     void Reposition()
     {
-        // 현재 위치에서 오른쪽으로 가로 길이 * 2만큼 이동
-        Vector2 offset = new Vector2(width * 2f, 0);
-        this.transform.position = (Vector2)this.transform.position + offset;
+        // 현재 위치에서 오른쪽으로 가로 길이 2배만큼 이동
+        Vector2 offset = new Vector2(width, 0);
+        try
+        {
+            Vector2 position = new Vector2(currentPlatforms[currentIndex + 2].transform.position.x, platformPositionY);
+            currentPlatforms[currentIndex + 2].transform.position = position + offset;
+            Debug.Log("다음 플랫폼 위치 옮김");
+        }
+        catch
+        {
+            Debug.Log("다음 플랫폼 없음");
+        }        
+    }
+
+    void RepositionPlatforms()
+    {
+        Vector2 offset = new Vector2(width, 0);
+
+        for (int i = 1; i < currentPlatforms.Length; i++)
+        {
+            Vector2 position = new Vector2(currentPlatforms[i].transform.position.x, platformPositionY);
+            currentPlatforms[i].transform.position = position + offset * i;
+            Debug.Log("다음 플랫폼 위치 옮김");
+        }        
     }
 }
